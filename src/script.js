cf4762fd4097ea9e9cfca25feb376b8aefe056f2
Maddddy2582 <madav.mahesh@solitontech.com>
2025-11-06 10:56:18 +0530

    changes

--- src/script.js
+++ src/script.js
@@ -17,13 +17,13 @@ const uv2Geometry = new THREE.BufferAttribute(geometry.attributes.uv.array, 2)
 geometry.setAttribute('uv2', uv2Geometry)
 
 // load the grass textures
-const grassAlbedo = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_albedo.png')
-grassAlbedo.colorSpace = THREE.SRGBColorSpace
-const grassAo = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_ao.png')
-const grassHeight = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_height.png')
-const grassMetallic = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_metallic.png')
-const grassNormal = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_normal-ogl.png')
-const grassRoughness = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_roughness.png')
+// const grassAlbedo = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_albedo.png')
+// grassAlbedo.colorSpace = THREE.SRGBColorSpace
+// const grassAo = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_ao.png')
+// const grassHeight = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_height.png')
+// const grassMetallic = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_metallic.png')
+// const grassNormal = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_normal-ogl.png')
+// const grassRoughness = textureLoader.load('/textures/whispy-grass-meadow-bl/wispy-grass-meadow_roughness.png')
 
 // load the boulder textures
 const boulderAlbedo = textureLoader.load('/textures/badlands-boulders-bl/badlands-boulders_albedo.png')
@@ -49,19 +49,19 @@ const grassPane = pane.addFolder({
   expanded: true
 })
 
-const grassMaterial = new THREE.MeshStandardMaterial();
-grassMaterial.map = grassAlbedo
-grassMaterial.roughnessMap = grassRoughness
-grassMaterial.metalnessMap = grassMetallic
-grassMaterial.normalMap = grassNormal
-grassMaterial.displacementMap = grassHeight
-grassMaterial.displacementScale = 0.1
-grassMaterial.aoMap = grassAo
+// const grassMaterial = new THREE.MeshStandardMaterial();
+// grassMaterial.map = grassAlbedo
+// grassMaterial.roughnessMap = grassRoughness
+// grassMaterial.metalnessMap = grassMetallic
+// grassMaterial.normalMap = grassNormal
+// grassMaterial.displacementMap = grassHeight
+// grassMaterial.displacementScale = 0.1
+// grassMaterial.aoMap = grassAo
 
-grassPane.addBinding(grassMaterial, 'metalness', { min: 0, max: 1, step: 0.01 })
-grassPane.addBinding(grassMaterial, 'roughness', { min: 0, max: 1, step: 0.01 })
-grassPane.addBinding(grassMaterial, 'displacementScale', { min: 0, max: 1, step: 0.01 })
-grassPane.addBinding(grassMaterial, 'aoMapIntensity', { min: 0, max: 1, step: 0.01 })
+// grassPane.addBinding(grassMaterial, 'metalness', { min: 0, max: 1, step: 0.01 })
+// grassPane.addBinding(grassMaterial, 'roughness', { min: 0, max: 1, step: 0.01 })
+// grassPane.addBinding(grassMaterial, 'displacementScale', { min: 0, max: 1, step: 0.01 })
+// grassPane.addBinding(grassMaterial, 'aoMapIntensity', { min: 0, max: 1, step: 0.01 })
 
 // boulder material
 const boulderPane = pane.addFolder({
@@ -108,9 +108,9 @@ const group = new THREE.Group()
 
 // initialize the mesh
 
-const grass = new THREE.Mesh()
-grass.geometry = geometry
-grass.material = grassMaterial
+// const grass = new THREE.Mesh()
+// grass.geometry = geometry
+// grass.material = grassMaterial
 
 const boulder = new THREE.Mesh()
 boulder.geometry = geometry
@@ -123,7 +123,7 @@ spaceCruiser.material = spaceCruiserMaterial
 spaceCruiser.position.x = -2.5
 
 // add the mesh to the scene
-group.add(grass, boulder, spaceCruiser )
+group.add( boulder, spaceCruiser )
 scene.add(group);
 
 // initialize the light
